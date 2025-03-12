f1618436833ac5e385a2ef9603cf092da1efb37f
Haoran Yin <h.yin@liacs.leidenuniv.nl>
2025-03-12 13:33:02 +0100

    population exp rerun

--- exp_scripts/CAI/post_CAI.py
+++ exp_scripts/CAI/post_CAI.py
@@ -29,24 +29,38 @@ def extract_auc(exp_folder):
     return aucs, final_y
 
 
-exps = ["bragg",
-        "bragg_with_description",
-        "bragg_with_description_insight",
-        "ellipsometry",
-        "ellipsometry_with_description",
-        "ellipsometry_with_description_insight"]
-labels = ["mini-bragg",
-          "mini-bragg with description",
-          "mini-bragg with description\nand algorithmic insights",
-          "ellipsometry",
-          "ellipsometry with description",
-          "ellipsometry with description\nand algorithmic insights"]
+exps = []
+labels = []
+exp_classes = ["(1, 5)", "(1 + 5)", "(2, 10)", "(2 + 10)", "(5 + 5)"]
+for exp_class in exp_classes:
+    exps += [
+        f"bragg_with_description_insight_{exp_class}",
+        # f"ellipsometry_with_description_insight_{exp_class}"
+    ]
+    labels += [
+        f"mini-bragg {exp_class}",
+        # f"ellipsometry {exp_class}"
+    ]
+
+# exps = ["bragg",
+#         "bragg_with_description",
+#         "bragg_with_description_insight",
+#         "ellipsometry",
+#         "ellipsometry_with_description",
+# #         "ellipsometry_with_description_insight"]
+# labels = ["mini-bragg",
+#           "mini-bragg with description",
+#           "mini-bragg with description\nand algorithmic insights",
+#           "ellipsometry",
+#           "ellipsometry with description",
+#           "ellipsometry with description\nand algorithmic insights"]
 cud = ["#e69f00", "#56b4e9", "#009e73", "#f0e442",
        "#0072b2", "#d55e00", "#cc79a7", "#000000"]
 linestyles = ["-", "--", "-.", ":", "-", "--", "-.", ":"]
 auc_data = []
 y_data = []
-exp_folders = os.listdir(f"exp_data/CAI/descriptions_insights/")
+root_path = "exp_data/CAI/populations/"
+exp_folders = os.listdir(root_path)
 for exp in exps:
     aucs = []
     ys = []
@@ -54,7 +68,7 @@ for exp in exps:
         contents = exp_folder.split("-")
         if contents[-1] != exp:
             continue
-        auc, y = extract_auc(f"exp_data/CAI/descriptions_insights/{exp_folder}")
+        auc, y = extract_auc(f"{root_path}{exp_folder}")
         if auc == None or len(auc) < 100:
             print(f"Skipping {exp_folder}")
             continue
@@ -75,23 +89,31 @@ for k in range(len(current_best_aucs)):
             values += [[j, array[i, j], y[i, j], i, labels[k]]]
 df = pd.DataFrame(values, columns=keys)
 df.to_csv("exp_data/CAI/real/conv_plot_description_insight_1.csv")
+
+df_baseline = pd.read_csv("exp_data/CAI/real/baselines.csv")
+df_baseline_label = df_baseline[df_baseline["problem"] == "mini-bragg (1 + 1)"]
+
+sns.lineplot(x="generation", y="auc", data=df_baseline_label,
+             color=cud[5], label="mini-bragg (1 + 1)")
 df = pd.read_csv("exp_data/CAI/real/conv_plot_description_insight_1.csv")
 for i, label in enumerate(labels):
     df_label = df[df["problem"] == label]
     sns.lineplot(x="generation", y="auc", data=df_label,
-                 label=label, linestyle=linestyles[int(i/3)], color=cud[i%3])
+                 label=label, linestyle=linestyles[int(i/5)], color=cud[i%5])
 # plt.yscale("log")
 plt.ylabel("AOCC")
 plt.legend()
-plt.savefig("results/CAI/auc_description_insight.png")
+plt.savefig("results/CAI/populations_bragg_auc_description_insight.png")
 plt.cla()
 
+sns.lineplot(x="generation", y="final_y", data=df_baseline_label,
+             color=cud[5], label="mini-bragg (1 + 1)")
 for i, label in enumerate(labels):
     df_label = df[df["problem"] == label]
     sns.lineplot(x="generation", y="final_y", data=df_label,
-                 label=label, linestyle=linestyles[int(i/3)], color=cud[i%3])
+                 label=label, linestyle=linestyles[int(i/5)], color=cud[i%5])
 plt.yscale("log")
 plt.ylabel(r"$y^*$")
 plt.legend()
-plt.savefig("results/CAI/y_description_insight.png")
+plt.savefig("results/CAI/populations_bragg_y_description_insight.png")
 plt.cla()
