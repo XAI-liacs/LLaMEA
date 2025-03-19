398e84c306f803dc2e756a805f4f552c6e00c634
Haoran Yin <h.yin@liacs.leidenuniv.nl>
2025-03-19 12:17:25 +0100

    benchmark data

--- exp_scripts/CAI/post_CAI.py
+++ exp_scripts/CAI/post_CAI.py
@@ -30,21 +30,136 @@ def extract_auc(exp_folder):
         return None, None
     return aucs, final_y
 
-problems = [
-    "bragg",
-    "ellipsometry",
-    "photovoltaic"
-]
-exps = []
-labels = []
 
-for prob in problems:
-    exps += [f"{prob}_(1 + 1)",
-                f"{prob}_with_description_(1 + 1)",
-                f"{prob}_with_description_insight_(1 + 1)"]
-    labels += [f"{prob}",
-               f"{prob} with description",
-               f"{prob} with description\nand algorithmic insights"]
+
+def plot(root_folder_path, exp_names, labels, plot_name):
+    cud = ["#e69f00", "#56b4e9", "#009e73", "#f0e442",
+        "#0072b2", "#d55e00", "#cc79a7", "#000000"]
+    linestyles = ["-", "--", ":", "-.", "-", "--", "-.", ":"]
+    auc_data = []
+    y_data = []
+    exp_folders = os.listdir(root_folder_path)
+    for exp in exp_names:
+        aucs = []
+        ys = []
+        for exp_folder in exp_folders:
+            contents = exp_folder.split("-")
+            if contents[-1] != exp:
+                continue
+            auc, y = extract_auc(f"{root_folder_path}{exp_folder}")
+            print(f"Extracting {exp_folder}")
+            if auc == None or len(auc) < 100:
+                print(f"Skipping {exp_folder}")
+                continue
+            aucs += [auc]
+            ys += [y]
+        max_len = max([len(auc) for auc in aucs])
+        aucs = [auc + [auc[-1]]*(max_len - len(auc)) for auc in aucs]
+        auc_data += [np.array(aucs)]
+        y_data += [np.array(ys)]
+    current_best_aucs = [np.maximum.accumulate(aucs, axis=1) for aucs in auc_data]
+    keys = ["generation", "auc", "final_y", "run", "problem"]
+    values = []
+    for k in range(len(current_best_aucs)):
+        array = current_best_aucs[k]
+        y = y_data[k]
+        for i in range(len(y)):
+            for j in range(100):
+                values += [[j, array[i, j], y[i, j], i, labels[k]]]
+    df = pd.DataFrame(values, columns=keys)
+    df.to_csv("exp_data/CAI/real/temp.csv")
+    df_baseline = pd.read_csv("exp_data/CAI/real/baselines.csv")
+    if "bragg" in plot_name:
+        df_baseline_label = df_baseline[df_baseline["problem"] == "mini-bragg (1 + 1)"]
+    elif "ellipsometry" in plot_name:
+        df_baseline_label = df_baseline[df_baseline["problem"] == "ellipsometry (1 + 1)"]
+    else:
+        df_baseline_label = df_baseline[df_baseline["problem"] == "photovoltaic (1 + 1)"]
+
+    sns.lineplot(x="generation", y="auc", data=df_baseline_label,
+                 color=cud[5], label="ES (1 + 1)", linestyle="--")
+    df = pd.read_csv("exp_data/CAI/real/temp.csv")
+    for i, label in enumerate(labels):
+        df_label = df[df["problem"] == label]
+        sns.lineplot(x="generation", y="auc", data=df_label,
+                    label=label, linestyle=linestyles[i], color=cud[i])
+    # plt.yscale("log")
+    plt.ylabel("AOCC")
+    plt.tight_layout()
+    plt.legend()
+    plt.savefig(f"results/CAI/auc_description_insight_{plot_name}.png")
+    plt.cla()
+
+    for i, label in enumerate(labels):
+        df_label = df[df["problem"] == label]
+        sns.lineplot(x="generation", y="final_y", data=df_label,
+                    label=label, linestyle=linestyles[i], color=cud[i])
+    plt.yscale("log")
+    plt.ylabel(r"$y^*$")
+    plt.tight_layout()
+    plt.legend()
+    plt.savefig(f"results/CAI/y_description_insight_{plot_name}.png")
+    plt.cla()
+
+def plot_description_insights():
+    problems = [
+        "bragg",
+        "ellipsometry",
+        "photovoltaic"
+    ]
+    for prob in problems:
+        exp_names = [
+            f"{prob}_(1 + 1)",
+            f"{prob}_with_description_(1 + 1)",
+            f"{prob}_with_description_insight_(1 + 1)"]
+        labels = [
+            f"{prob}",
+            f"{prob} with description",
+            f"{prob} with description\nand algorithmic insights"]
+        plot("exp_data/CAI/descriptions_insights/", exp_names, labels, prob)
+        if prob == "photovoltaic":
+            plot("exp_data/CAI/descriptions_insights/start_with_same_solution/",
+                 exp_names, labels, "photovoltaic_start_with_same_solution")
+
+
+def plot_population():
+    problems = [
+        "bragg",
+        "ellipsometry",
+        "photovoltaic"
+    ]
+    for prob in problems:
+        if prob == "photovoltaic":
+            exp_names = [
+                f"{prob}_(1, 5)",
+                f"{prob}_(1 + 5)",
+                f"{prob}_(2, 10)",
+                f"{prob}_(2 + 10)",
+                f"{prob}_(5 + 5)",
+            ]
+        else:
+            exp_names = [
+                f"{prob}_with_description_insight_(1, 5)",
+                f"{prob}_with_description_insight_(1 + 5)",
+                f"{prob}_with_description_insight_(2, 10)",
+                f"{prob}_with_description_insight_(2 + 10)",
+                f"{prob}_with_description_insight_(5 + 5)",
+            ]
+        labels = [
+            f"ES (1 , 5)",
+            f"ES (1 + 5)",
+            f"ES (2 , 10)",
+            f"ES (2 + 10)",
+            f"ES (5 + 5)",
+        ]
+        plot("exp_data/CAI/populations/", exp_names, labels, f"{prob}_population")
+        # if prob == "photovoltaic":
+        #     plot("exp_data/CAI/descriptions_insights/start_with_same_solution/",
+        #          exp_names, labels, "photovoltaic_start_with_same_solution")
+
+
+# plot_description_insights()
+plot_population()
 
 # exps = []
 # labels = []
@@ -71,76 +186,3 @@ for prob in problems:
 #           "ellipsometry",
 #           "ellipsometry with description",
 #           "ellipsometry with description\nand algorithmic insights"]
-cud = ["#e69f00", "#56b4e9", "#009e73", "#f0e442",
-       "#0072b2", "#d55e00", "#cc79a7", "#000000"]
-linestyles = ["-", "--", "-.", ":", "-", "--", "-.", ":"]
-auc_data = []
-y_data = []
-root_path = "exp_data/CAI/descriptions_insights/"
-exp_folders = os.listdir(root_path)
-
-for exp in exps:
-    aucs = []
-    ys = []
-    for exp_folder in exp_folders:
-        contents = exp_folder.split("-")
-        if contents[-1] != exp:
-            continue
-        auc, y = extract_auc(f"{root_path}{exp_folder}")
-        print(f"Extracting {exp_folder}")
-        if auc == None or len(auc) < 100:
-            print(f"Skipping {exp_folder}")
-            continue
-        aucs += [auc]
-        ys += [y]
-    print([len(auc) for auc in aucs])
-    max_len = max([len(auc) for auc in aucs])
-    aucs = [auc + [auc[-1]]*(max_len - len(auc)) for auc in aucs]
-    auc_data += [np.array(aucs)]
-    y_data += [np.array(ys)]
-current_best_aucs = [np.maximum.accumulate(aucs, axis=1) for aucs in auc_data]
-# print(auc_data[0][0])
-# print(current_best_aucs[0][0])
-keys = ["generation", "auc", "final_y", "run", "problem"]
-values = []
-for k in range(len(current_best_aucs)):
-    array = current_best_aucs[k]
-    y = y_data[k]
-    for i in range(len(y)):
-        for j in range(100):
-            values += [[j, array[i, j], y[i, j], i, labels[k]]]
-# print(np.array(values)[:100, 1])
-df = pd.DataFrame(values, columns=keys)
-df.to_csv("exp_data/CAI/real/conv_plot_description_insight_1.csv")
-
-df_baseline = pd.read_csv("exp_data/CAI/real/baselines.csv")
-df_baseline_label = df_baseline[df_baseline["problem"] == "mini-bragg (1 + 1)"]
-
-# sns.lineplot(x="generation", y="auc", data=df_baseline_label,
-#              color=cud[5], label="mini-bragg (1 + 1)")
-df = pd.read_csv("exp_data/CAI/real/conv_plot_description_insight_1.csv")
-for problem in problems:
-    for i, label in enumerate(labels):
-        if problem not in label:
-            continue
-        df_label = df[df["problem"] == label]
-        print(df_label)
-        sns.lineplot(x="generation", y="auc", data=df_label,
-                    label=label, linestyle=linestyles[int(i/3)], color=cud[i%3])
-    # plt.yscale("log")
-    plt.ylabel("AOCC")
-    plt.legend()
-    plt.savefig(f"results/CAI/auc_description_insight_{problem}.png")
-    plt.cla()
-
-    for i, label in enumerate(labels):
-        if problem not in label:
-            continue
-        df_label = df[df["problem"] == label]
-        sns.lineplot(x="generation", y="final_y", data=df_label,
-                    label=label, linestyle=linestyles[int(i/3)], color=cud[i%3])
-    plt.yscale("log")
-    plt.ylabel(r"$y^*$")
-    plt.legend()
-    plt.savefig(f"results/CAI/y_description_insight_{problem}.png")
-    plt.cla()
--- exp_scripts/CAI/run_best_algs.py
+++ exp_scripts/CAI/run_best_algs.py
@@ -1,6 +1,7 @@
-
+import os
 import sys
 import ioh
+import jsonlines
 import numpy as np
 import best_algs.best1 as best1
 import best_algs.best2 as best2
@@ -8,83 +9,197 @@ import best_algs.best3 as best3
 sys.path.append(".")
 sys.path.append("./benchmarks/tuto_global_optimization_photonics/")
 from photonics_benchmark import *
+from misc import OverBudgetException
 
-def get_photonic_instances():
-    problems = []
-    # ------- define "mini-bragg" optimization problem
-    nb_layers = 20     # number of layers of full stack
-    target_wl = 600.0  # nm
-    mat_env = 1.0      # materials: ref. index
-    mat1 = 1.4
-    mat2 = 1.8
-    prob = brag_mirror(nb_layers, target_wl, mat_env, mat1, mat2)
-    ioh.problem.wrap_real_problem(prob, name="brag_mirror",
-                                  optimization_type=ioh.OptimizationType.MIN)
-    problem = ioh.get_problem("brag_mirror", dimension=prob.n)
-    problem.bounds.lb = prob.lb
-    problem.bounds.ub = prob.ub
-    problems.append(problem)
-    # ------- define "ellipsometry" optimization problem
-    mat_env = 1.0
-    mat_substrate = 'Gold'
-    nb_layers = 1
-    min_thick = 50     # nm
-    max_thick = 150
-    min_eps = 1.1      # permittivity
-    max_eps = 3
-    wavelengths = np.linspace(400, 800, 100)  # nm
-    angle = 40*np.pi/180  # rad
-    prob = ellipsometry(mat_env, mat_substrate, nb_layers, min_thick, max_thick,
-                        min_eps, max_eps, wavelengths, angle)
-    ioh.problem.wrap_real_problem(prob, name="ellipsometry",
-                                  optimization_type=ioh.OptimizationType.MIN,)
-    problem = ioh.get_problem("ellipsometry", dimension=prob.n)
-    problem.bounds.lb = prob.lb
-    problem.bounds.ub = prob.ub
-    problems.append(problem)
-    # ------- define "sophisticated antireflection" optimization problem
-    nb_layers = 10
-    min_thick = 30
-    max_thick = 250
-    wl_min = 375
-    wl_max = 750
-    prob = sophisticated_antireflection_design(nb_layers, min_thick, max_thick,
-                                               wl_min, wl_max)
-    ioh.problem.wrap_real_problem(prob, name="sophisticated_antireflection_design",
-                                  optimization_type=ioh.OptimizationType.MIN)
-    problem = ioh.get_problem("sophisticated_antireflection_design",
-                              dimension=prob.n)
-    problem.bounds.lb = prob.lb
-    problem.bounds.ub = prob.ub
-    problems.append(problem)
-    return problems
+def get_photonic_instances(instance_id):
+    if instance_id == 0:
+        # ------- define "mini-bragg" optimization problem
+        nb_layers = 10     # number of layers of full stack
+        target_wl = 600.0  # nm
+        mat_env = 1.0      # materials: ref. index
+        mat1 = 1.4
+        mat2 = 1.8
+        prob = brag_mirror(nb_layers, target_wl, mat_env, mat1, mat2)
+        ioh.problem.wrap_real_problem(prob, name="mini_bragg",
+                                      optimization_type=ioh.OptimizationType.MIN)
+        problem = ioh.get_problem("mini_bragg", dimension=prob.n)
+        problem.bounds.lb = prob.lb
+        problem.bounds.ub = prob.ub
+        return problem
+    elif instance_id == 1:
+        # ------- define "mini-bragg" optimization problem
+        nb_layers = 20     # number of layers of full stack
+        target_wl = 600.0  # nm
+        mat_env = 1.0      # materials: ref. index
+        mat1 = 1.4
+        mat2 = 1.8
+        prob = brag_mirror(nb_layers, target_wl, mat_env, mat1, mat2)
+        ioh.problem.wrap_real_problem(prob, name="bragg",
+                                      optimization_type=ioh.OptimizationType.MIN)
+        problem = ioh.get_problem("bragg", dimension=prob.n)
+        problem.bounds.lb = prob.lb
+        problem.bounds.ub = prob.ub
+        return problem
+    elif instance_id == 2:
+        # ------- define "ellipsometry" optimization problem
+        mat_env = 1.0
+        mat_substrate = 'Gold'
+        nb_layers = 1
+        min_thick = 50     # nm
+        max_thick = 150
+        min_eps = 1.1      # permittivity
+        max_eps = 3
+        wavelengths = np.linspace(400, 800, 100)  # nm
+        angle = 40*np.pi/180  # rad
+        prob = ellipsometry(mat_env, mat_substrate, nb_layers, min_thick, max_thick,
+                            min_eps, max_eps, wavelengths, angle)
+        ioh.problem.wrap_real_problem(prob, name="ellipsometry",
+                                      optimization_type=ioh.OptimizationType.MIN,)
+        problem = ioh.get_problem("ellipsometry", dimension=prob.n)
+        problem.bounds.lb = prob.lb
+        problem.bounds.ub = prob.ub
+        return problem
+    elif instance_id == 3:
+        # ------- define "sophisticated antireflection" optimization problem
+        nb_layers = 10
+        min_thick = 30
+        max_thick = 250
+        wl_min = 375
+        wl_max = 750
+        prob = sophisticated_antireflection_design(nb_layers, min_thick, max_thick,
+                                                wl_min, wl_max)
+        ioh.problem.wrap_real_problem(prob, name="photovoltaic", 
+                                      optimization_type=ioh.OptimizationType.MIN)
+        problem = ioh.get_problem("photovoltaic", dimension=prob.n)
+        problem.bounds.lb = prob.lb
+        problem.bounds.ub = prob.ub
+        return problem
+    elif instance_id == 4:
+        # ------- define "sophisticated antireflection" optimization problem
+        nb_layers = 20
+        min_thick = 30
+        max_thick = 250
+        wl_min = 375
+        wl_max = 750
+        prob = sophisticated_antireflection_design(nb_layers, min_thick, max_thick,
+                                                wl_min, wl_max)
+        ioh.problem.wrap_real_problem(prob, name="big_photovoltaic",
+                                      optimization_type=ioh.OptimizationType.MIN)
+        problem = ioh.get_problem("big_photovoltaic", dimension=prob.n)
+        problem.bounds.lb = prob.lb
+        problem.bounds.ub = prob.ub
+        return problem
+    elif instance_id == 5:
+        # ------- define "sophisticated antireflection" optimization problem
+        nb_layers = 32
+        min_thick = 30
+        max_thick = 250
+        wl_min = 375
+        wl_max = 750
+        prob = sophisticated_antireflection_design(nb_layers, min_thick, max_thick,
+                                                wl_min, wl_max)
+        ioh.problem.wrap_real_problem(prob, name="huge_photovoltaic",
+                                      optimization_type=ioh.OptimizationType.MIN)
+        problem = ioh.get_problem("huge_photovoltaic", dimension=prob.n)
+        problem.bounds.lb = prob.lb
+        problem.bounds.ub = prob.ub
+        return problem
 
-def run_experiment(problem, n_runs=5):
-    for run in range(n_runs):
-        algorithm = best1.EnhancedHybridPSO_DE(budget, dim)
-        # Run the algorithm on the problem
-        algorithm(problem)
-        # print the best found for this run
-        print(f"run: {run+1} - best found:{problem.state.current_best.y: .3f}")
-        # Reset the problem
-        problem.reset()
 
-problems = get_photonic_instances()
-f = problems[0]
-dim = f.meta_data.n_variables
-budget = 1000 * dim
-print(f.state)
-print(f.meta_data)
-print(f.bounds)
-logger_params = dict(
-    triggers=[ioh.logger.trigger.ALWAYS, ioh.logger.trigger.ON_VIOLATION],
-    additional_properties=[],
-    root="exp_data/CAI/",
-    folder_name="bragg",
-    algorithm_name='best_alg1',
-    store_positions=True,
-)
-logger = ioh.logger.Analyzer(**logger_params)
-logger.reset()
-f.attach_logger(logger)
-run_experiment(f, n_runs=15)
+def find_best_algs():
+    n_algs = 10
+    problems = [
+        "bragg",
+        "ellipsometry",
+        "photovoltaic"
+    ]
+    best_alg_fitness = [[0. for _ in range(n_algs)] for _ in range(len(problems))]
+    best_alg_solution = [[None for _ in range(n_algs)] for _ in range(len(problems))]
+    best_alg_name = [[None for _ in range(n_algs)] for _ in range(len(problems))]
+    root_path = "exp_data/CAI/populations/"
+    folders = os.listdir(root_path)
+    for folder in folders:
+        folder_problem = ""
+        if not folder.startswith("exp"):
+            continue
+        for problem in problems:
+            if problem in folder:
+                folder_problem = problem
+                break
+        if folder_problem == "":
+            print(f"Skipping {folder}")
+        folder_problem_index = problems.index(folder_problem)
+        log_file = f"{root_path}{folder}/log.jsonl"
+        with jsonlines.open(log_file) as reader:
+            for obj in reader:
+                fitness = obj["fitness"]
+                solution = obj["solution"]
+                name = obj["name"]
+                for i in range(n_algs):
+                    if fitness > best_alg_fitness[folder_problem_index][i]:
+                        for j in range(n_algs-1, i, -1):
+                            best_alg_fitness[folder_problem_index][j] = \
+                                best_alg_fitness[folder_problem_index][j-1]
+                            best_alg_solution[folder_problem_index][j] = \
+                                best_alg_solution[folder_problem_index][j-1]
+                            best_alg_name[folder_problem_index][j] = \
+                                best_alg_name[folder_problem_index][j-1]
+                        best_alg_fitness[folder_problem_index][i] = fitness
+                        best_alg_solution[folder_problem_index][i] = solution
+                        best_alg_name[folder_problem_index][i] = name
+                        break
+    return best_alg_fitness, best_alg_solution, best_alg_name
+
+
+def benchmark(best_alg_name, best_alg_solution, instance_id, n_runs=15):
+    alg_ids = [
+        [3, 4, 6],
+        [0, 1, 2],
+        [2]
+    ]
+    best_alg_group_id = -1
+    if instance_id in [0, 1]:
+        best_alg_group_id = 0
+    elif instance_id in [2]:
+        best_alg_group_id = 1
+    elif instance_id in [3, 4, 5]:
+        best_alg_group_id = 2
+    if best_alg_group_id == -1:
+        print("Instance ID not found")
+        return None
+    for i in alg_ids[best_alg_group_id]:
+        code = best_alg_solution[best_alg_group_id][i]
+        algorithm_name = best_alg_name[best_alg_group_id][i]
+        exec(code, globals())
+        problem = get_photonic_instances(instance_id)
+        logger_params = dict(
+            triggers=[ioh.logger.trigger.ALWAYS, ioh.logger.trigger.ON_VIOLATION],
+            additional_properties=[],
+            root="exp_data/CAI/benchmark/",
+            folder_name=f"{problem.meta_data.name}_best_alg{i}_{algorithm_name}",
+            algorithm_name=f"best_alg{i}_{algorithm_name}",
+            store_positions=True,
+        )
+        logger = ioh.logger.Analyzer(**logger_params)
+        logger.reset()
+        problem.attach_logger(logger)
+        dim = problem.meta_data.n_variables
+        if instance_id in [0, 1]:
+            budget = 1000 * dim
+        elif instance_id in [2, 3, 4, 5]:
+            budget = 500 * dim
+        for run in range(n_runs):
+            try:
+                algorithm = globals()[algorithm_name](budget=budget, dim=dim)
+                algorithm(problem)
+                print(f"run: {run+1} - best found:{problem.state.current_best.y: .3f}")
+                problem.reset()
+            except OverBudgetException:
+                pass
+
+
+if __name__ == "__main__":
+    best_alg_fitness, best_alg_solution, best_alg_name = find_best_algs()
+    print(best_alg_name)
+    for instance_id in range(5, 6):
+        benchmark(best_alg_name, best_alg_solution, instance_id)
